021755d6e36953ccb6bce360da51a3b68a07b00a
Yuri <davydiuk.yurii@gmail.com>
2025-12-07 22:28:06 +0200

    add: js-task

---
+++ js/1-2 modules/task-6.js
@@ -0,0 +1,10 @@
+// Використайте цикл while, щоб вивести в console всі числа від 0 до 20 включно.
+
+function number() {
+  let i = 0;
+  while (i < 20) {
+    console.log('The current value of i is: ' + i);
+    i++;
+  }
+}
+number();
---
+++ js/1-2 modules/task-8.js
@@ -0,0 +1,17 @@
+// Напишіть функцію min(a, b), яка приймає 2 числа і повертає меньше з них.
+// Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
+// повертати з функції рядок - 'Not a number!'.
+
+function min(a, b) {
+  if (
+    typeof a !== 'number' ||
+    typeof b !== 'number' ||
+    Number.isNaN(a) ||
+    Number.isNaN(b)
+  ) {
+    return 'Not a number!';
+  }
+  return a < b ? a : b;
+}
+
+min(2, 5);
